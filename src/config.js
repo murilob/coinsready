ccafd0fb191523176bfcbb7f412d1b4ae686ec64
t318200 <murilo.costa@tjdft.jus.br>
2018-08-01 19:26:03 -0300

    Adjusting config file with addresses and referral code.

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const config = {
-  NAME: 'N.exchange',
-  DOMAIN: 'https://n.exchange',
+  NAME: 'Coinsready',
+  DOMAIN: 'https://coinsready.io',
   API_BASE_URL: 'https://api.nexchange.io/en/api/v1', // 'https://api.nexchange.io/en/api/v1', // http://207.154.223.232:8000/en/api/v1
-  SUPPORT_EMAIL: 'support@n.exchange',
+  SUPPORT_EMAIL: 'support@coinsready.io',
   PRICE_FETCH_INTERVAL: 60000,
   ORDER_DETAILS_FETCH_INTERVAL: 20000,
   RECENT_ORDERS_INTERVAL: 20000,
   RECENT_ORDERS_COUNT: 11,
   PRICE_COMPARISON_INTERVAL: 60000,
-  REFERRAL_CODE: null,
+  REFERRAL_CODE: 'RVRZW47XVEU',
   KYC_DETAILS_FETCH_INTERVAL: 20000,
 };
 
